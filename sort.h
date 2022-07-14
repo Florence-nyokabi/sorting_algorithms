cab098efe8a4b59016e1fb07c8bb13fe24089b99
Florence-wangui <wanguif018@gmail.com>
2022-07-14 03:35:39 -0700

    sort.h

--- sort.h
+++ sort.h
@@ -1,3 +1,8 @@
+#ifndef _SORT_H
+#define _SORT_H
+
+#include <stdio.h>
+#include "sort.h"
 
 /**
  * struct listint_s - Doubly linked list node
@@ -12,3 +17,26 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+/* PROTOTPES FOR PRINT FUNCTIONS */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* Prototypes for mandatory tasks */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+
+/* Prototypes for advanced tasks */
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
+
+#endif /* sort.h */
