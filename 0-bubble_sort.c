a9bf6ff253efe610a6b71e8b2a0b7ee25bb3eb52
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-07-16 18:51:11 +0300

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,29 +1,23 @@
 #include "sort.h"
+
 /**
- * bubble_sort - Sort an array with buble sort algorithm
- * @array: Array that going to be sorted
- * @size: Size of array
- * Return: Nothing, just sort and print when swap
+ * bubble_sort - a function to sort an array
+ * @array: integer array to be sorted
+ * @size: size of the array
  */
+
 void bubble_sort(int *array, size_t size)
 {
-	int tmp;
 	size_t i, j;
-
-	if (array == NULL || size == 0)
-		return;
+	int temp;
 
 	for (i = 0; i < size; i++)
-	{
 		for (j = 0; j < size - 1; j++)
-		{
 			if (array[j] > array[j + 1])
 			{
-				tmp = array[j];
+				temp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
-		}
-	}
 }
