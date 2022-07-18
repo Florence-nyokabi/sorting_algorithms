aef70cf0ba2c19e6c0accc1c51219c4380c77b2b
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-07-18 13:14:56 +0300

    sort.h

--- sort.h
+++ sort.h
@@ -1,7 +1,23 @@
 #ifndef _SORT_H_
 #define _SORT_H_
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
+
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
